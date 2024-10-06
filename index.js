05cc46b75764adb426be23a67be1129866dd1d2b
John07mrn <john1992miron@gmail.com>
2024-10-06 13:51:23 +0100

    add Array of Objects & Date

--- index.js
+++ index.js
@@ -19,3 +19,62 @@ let result = fruits2.indexOf('grapes') !== -1;
 console.log(result); // Displays: false (because 'grapes' is not in the array)
 
 
+//P3
+// Create a Date object
+let currentDate = new Date();
+
+// Display the current year in the console
+console.log(currentDate.getFullYear()); // Displays the current year
+
+
+//P4
+// Create a Date object
+let currentDate2 = new Date();
+
+// Add 5 days to the current date
+currentDate2.setDate(currentDate2.getDate() + 5);
+
+// Display the new date in the console
+console.log(currentDate2);
+
+
+//P5
+// Create two Date objects representing two points in time
+let timestamp1 = new Date('2020-01-07').getTime();
+let timestamp2 = new Date('2020-08-16').getTime();
+
+// Calculate the difference between them in milliseconds
+let differenceInMilliseconds = timestamp2 - timestamp1;
+
+// Display the result in the console
+console.log(differenceInMilliseconds);
+
+
+//P6
+// Create an object with information about yourself
+let person = { name: 'John Mrn', age: 32, city: 'Milton Keynes' };
+
+// Display the object in the console
+console.log(person);
+
+
+//P7
+// Modify the age of the person from the previously created object
+person.age = 25;
+
+// Display the modified object in the console
+console.log(person);
+
+
+//P8
+// Create an array of objects representing books
+let books = [
+    { title: 'Book1', author: 'Author1', yearPublished: 2005 },
+    { title: 'Book2', author: 'Author2', yearPublished: 1997 }
+  ];
+  
+// Use an array method to add another object representing a book
+books.push({ title: 'Book3', author: 'Author3', yearPublished: 2023 });
+
+// Display the array of books in the console
+console.log(books);
